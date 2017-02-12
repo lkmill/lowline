04f735eedddb9af83461c704932ec8d950f86cf4
Linus Miller <lohfu@lohfu.io>
2017-02-12 01:05:02 +0100

    Fix eslint errors

--- src/isPlainObject.js
+++ src/isPlainObject.js
@@ -9,21 +9,19 @@
  *  @returns {Boolean} true if passes tests, false otherwise
  */
 export default function isPlainObject(obj) {
-
   // Basic check for Type object that's not null
-  if (typeof obj == 'object' && obj !== null) {
-
+  if (typeof obj === 'object' && obj !== null) {
     // If Object.getPrototypeOf supported, use it
-    if (typeof Object.getPrototypeOf == 'function') {
-      var proto = Object.getPrototypeOf(obj);
+    if (typeof Object.getPrototypeOf === 'function') {
+      const proto = Object.getPrototypeOf(obj);
       return proto === Object.prototype || proto === null;
     }
-    
+
     // Otherwise, use internal class
     // This should be reliable as if getPrototypeOf not supported, is pre-ES5
-    return Object.prototype.toString.call(obj) == '[object Object]';
+    return Object.prototype.toString.call(obj) === '[object Object]';
   }
-  
+
   // Not an object
   return false;
 }
