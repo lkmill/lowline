c3b513e66bceb5bd4331462852b892a06b1dcda1
Linus Miller <lohfu@lohfu.io>
2017-05-04 23:30:35 +0200

    Fix set

--- src/set.js
+++ src/set.js
@@ -1,13 +1,21 @@
-export default function set(obj, path, value) {
-  if (!obj || typeof path !== 'string') return;
+import isObject from './isObject';
 
-  path = path.split('.');
+export default function set(obj, path, value) {
+  if (!obj || path == null || !path.length) return;
+
+  if (typeof path === 'string') {
+    path = path.split('.');
+  }
 
   let attr;
 
   while ((attr = path.shift()) != null) {
     if (path.length) {
-      obj = obj[attr] = {};
+      if (!isObject(obj[attr])) {
+        obj[attr] = {};
+      }
+
+      obj = obj[attr];
     } else {
       obj[attr] = value;
     }
