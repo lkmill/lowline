98805655273b2395c36c4762d83a7d2a56d96e31
Linus Miller <lohfu@lohfu.io>
2018-04-26 08:26:00 +0200

    Export everything has both default and named exports

--- src/index.mjs
+++ src/index.mjs
@@ -1,42 +1,132 @@
-export { default as _normalizeCase } from './_normalizeCase'
-export { default as after } from './after'
-export { default as bindAll } from './bindAll'
-export { default as camelCase } from './camelCase'
-export { default as defaults } from './defaults'
-export { default as each } from './each'
-export { default as entries } from './entries'
-export { default as first } from './first'
-export { default as forEach } from './forEach'
-export { default as get } from './get'
-export { default as has } from './has'
-export { default as head } from './head'
-export { default as invokeMap } from './invokeMap'
-export { default as initial } from './initial'
-export { default as isArray } from './isArray'
-export { default as isArrayLike } from './isArrayLike'
-export { default as isEmpty } from './isEmpty'
-export { default as isLength } from './isLength'
-export { default as isObject } from './isObject'
-export { default as isPlainObject } from './isPlainObject'
-export { default as kebabCase } from './kebabCase'
-export { default as keys } from './keys'
-export { default as last } from './last'
-export { default as map } from './map'
-export { default as mapKeys } from './mapKeys'
-export { default as mapValues } from './mapValues'
-export { default as merge } from './merge'
-export { default as omit } from './omit'
-export { default as omitBy } from './omitBy'
-export { default as pick } from './pick'
-export { default as pickBy } from './pickBy'
-export { default as reduce } from './reduce'
-export { default as result } from './result'
-export { default as set } from './set'
-export { default as snakeCase } from './snakeCase'
-export { default as startCase } from './startCase'
-export { default as throttle } from './throttle'
-export { default as toPairs } from './toPairs'
-export { default as uniq } from './uniq'
-export { default as unset } from './unset'
-export { default as values } from './values'
-export { default as without } from './without'
+import _normalizeCase from './_normalizeCase'
+import after from './after'
+import bindAll from './bindAll'
+import camelCase from './camelCase'
+import defaults from './defaults'
+import each from './each'
+import entries from './entries'
+import first from './first'
+import forEach from './forEach'
+import get from './get'
+import has from './has'
+import head from './head'
+import invokeMap from './invokeMap'
+import initial from './initial'
+import isArray from './isArray'
+import isArrayLike from './isArrayLike'
+import isEmpty from './isEmpty'
+import isLength from './isLength'
+import isObject from './isObject'
+import isPlainObject from './isPlainObject'
+import kebabCase from './kebabCase'
+import keys from './keys'
+import last from './last'
+import map from './map'
+import mapKeys from './mapKeys'
+import mapValues from './mapValues'
+import merge from './merge'
+import omit from './omit'
+import omitBy from './omitBy'
+import pick from './pick'
+import pickBy from './pickBy'
+import reduce from './reduce'
+import result from './result'
+import set from './set'
+import snakeCase from './snakeCase'
+import startCase from './startCase'
+import throttle from './throttle'
+import toPairs from './toPairs'
+import uniq from './uniq'
+import unset from './unset'
+import values from './values'
+import without from './without'
+
+export {
+  _normalizeCase,
+  after,
+  bindAll,
+  camelCase,
+  defaults,
+  each,
+  entries,
+  first,
+  forEach,
+  get,
+  has,
+  head,
+  invokeMap,
+  initial,
+  isArray,
+  isArrayLike,
+  isEmpty,
+  isLength,
+  isObject,
+  isPlainObject,
+  kebabCase,
+  keys,
+  last,
+  map,
+  mapKeys,
+  mapValues,
+  merge,
+  omit,
+  omitBy,
+  pick,
+  pickBy,
+  reduce,
+  result,
+  set,
+  snakeCase,
+  startCase,
+  throttle,
+  toPairs,
+  uniq,
+  unset,
+  values,
+  without
+}
+
+export default {
+  _normalizeCase,
+  after,
+  bindAll,
+  camelCase,
+  defaults,
+  each,
+  entries,
+  first,
+  forEach,
+  get,
+  has,
+  head,
+  invokeMap,
+  initial,
+  isArray,
+  isArrayLike,
+  isEmpty,
+  isLength,
+  isObject,
+  isPlainObject,
+  kebabCase,
+  keys,
+  last,
+  map,
+  mapKeys,
+  mapValues,
+  merge,
+  omit,
+  omitBy,
+  pick,
+  pickBy,
+  reduce,
+  result,
+  set,
+  snakeCase,
+  startCase,
+  throttle,
+  toPairs,
+  uniq,
+  unset,
+  values,
+  without
+}
